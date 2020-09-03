8f01cb9a8a94cbc6b01cf30bbc971513e1fa66f9
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-03 19:36:43 +0900

    Basic認証の導入(ローカル環境)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    @basic_user = Rails.application.credentials.basic_auth[:basic_auth_user]
+    @basic_password = Rails.application.credentials.basic_auth[:basic_auth_password]
+
+    authenticate_or_request_with_http_basic do |username, password|
+      username == @basic_user && password == @basic_password
+    end
+  end
 end
+ 
