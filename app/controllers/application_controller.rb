60459eb1701daf26ce3ce15a29767ed9fdbff7b3
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-05 14:09:55 +0900

    deviseのストロングパラメーターの設定（新規登録）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -10,6 +11,17 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == @basic_user && password == @basic_password
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :nickname,
+      :family_name,
+      :first_name,
+      :family_name_reading,
+      :first_name_reading,
+      :date_of_birth,
+    ])
   end
 end
  
