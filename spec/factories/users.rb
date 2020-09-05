5fac4cb039b42e4dacba9257ed7d9adad3870133
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-05 23:09:21 +0900

    UserモデルのFactoryBotの値設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              {"furima太郎"}
+    email                 {"sample@example.com"}
+    password              {"abc123"}
+    password_confirmation {password}
+    family_name           {"山田"}
+    first_name            {"陸太郎"}
+    family_name_reading   {"ヤマダ"}
+    first_name_reading    {"リクタロウ"}
   end
 end
