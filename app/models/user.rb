c9e6dbd1bf321ebd77108f476f8ae97970fb5b23
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-05 18:28:59 +0900

    Userモデルのパスワードのバリデーション修正(半角英数字)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
 
   #空の投稿を保存しない
   with_options presence: true do
-    validates :nickname
+    validates :nickname              #validationはフォームからやってきた情報のフィルタリングなので保存先のカラム名というよりフォーム名を記述
     validates :email
-    validates :encrypted_password,   length: { minimum:6 }, format: { with: /\A[a-zA-Z0-9]+\z/, message:"は6文字以上の半角英数字で入力してください。" }
+    validates :password,             format: { with: /\A[a-zA-Z0-9]+\z/, message:"は半角英数字で入力してください" }
     validates :family_name,          format: { with: /\A[ぁ-んァ-ン一-龥]/, message:"は全角で入力してください。" }
     validates :first_name,           format: { with: /\A[ぁ-んァ-ン一-龥]/, message:"は全角で入力してください。" }
     validates :family_name_reading,  format: { with: /\A[ァ-ヶー－]+\z/, message:"は全角カナで入力してください。" }
