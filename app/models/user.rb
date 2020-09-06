92c775599c2ab4f0940c493c11a52b9f50357d89
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-06 20:09:23 +0900

    Userモデルの正規表現の変数化

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable #:validatable
 
   with_options presence: true do
+
+    password_regexp = /\A[a-zA-Z0-9]+\z/
+    name_regexp = /\A[ぁ-んァ-ン一-龥]/
+    name_reading_regexp = /\A[ァ-ヶー－]+\z/
+
     validates :nickname
-    validates :email,                format: { with: /\A[^@\s]+@[^@\s]+\z/ }, uniqueness: { case_sensitive: false }
-    validates :password,             format: { with: /\A[a-zA-Z0-9]+\z/, message: "は半角英数字で入力してください。" }, length: { minimum:6 }
-    validates :password_confirmation,format: { with: /\A[a-zA-Z0-9]+\z/, message: "は半角英数字で入力してください。" }, length: { minimum:6 }
-    validates :family_name,          format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。" }
-    validates :first_name,           format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。" }
-    validates :family_name_reading,  format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カナで入力してください。" }
-    validates :first_name_reading,   format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カナで入力してください。" }
+    validates :email,                format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "は@を含めて入力してください。"}, uniqueness: { case_sensitive: false }
+    validates :password,             format: { with: password_regexp, message: "は半角英数字で入力してください。" }, length: { minimum:6 }
+    validates :password_confirmation,format: { with: password_regexp, message: "は半角英数字で入力してください。" }, length: { minimum:6 }
+    validates :family_name,          format: { with: name_regexp, message: "は全角で入力してください。" }
+    validates :first_name,           format: { with: name_regexp, message: "は全角で入力してください。" }
+    validates :family_name_reading,  format: { with: name_reading_regexp, message: "は全角カナで入力してください。" }
+    validates :first_name_reading,   format: { with: name_reading_regexp, message: "は全角カナで入力してください。" }
     validates :date_of_birth
   end
 end
