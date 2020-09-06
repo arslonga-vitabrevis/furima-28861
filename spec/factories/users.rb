abba8428cd23c6369209deedcee6af11a9ab33ce
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-06 16:44:17 +0900

    gem fakerの導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"furima太郎"}
-    email                 {"sample@example.com"}
-    password              {"abc123"}
+    nickname              {Faker::Internet.username}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    family_name           {"山田"}
-    first_name            {"陸太郎"}
-    family_name_reading   {"ヤマダ"}
-    first_name_reading    {"リクタロウ"}
+    family_name           {Faker::Japanese::Name.last_name}
+    first_name            {Faker::Japanese::Name.first_name}
+    family_name_reading   {Faker::Japanese::Name.last_name.yomi}
+    first_name_reading    {Faker::Japanese::Name.first_name.yomi}
+    date_of_birth         {Faker::Date.between(from:"1930-01-01", to:"2015-12-31")} 
   end
 end
