788292352dde708b59df41ea9c7fee023a259883
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-06 13:35:56 +0900

    Userモデルのバリデーションの修正
    emailの一意性制約には大文字と小文字の区別は無視するように変更。
    
    date_of_birthに保存されるデータは数値形では無く日付型なのでnumericalityを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,12 @@ class User < ApplicationRecord
   #空の投稿を保存しない
   with_options presence: true do
     validates :nickname              #validationはフォームからやってきた情報のフィルタリングなので保存先のカラム名というよりフォーム名を記述
-    validates :email,                uniqueness: true
+    validates :email,                uniqueness: { case_sensitive: false }
     validates :password,             format: { with: /\A[a-zA-Z0-9]+\z/, message:"は半角英数字で入力してください" }
     validates :family_name,          format: { with: /\A[ぁ-んァ-ン一-龥]/, message:"は全角で入力してください。" }
     validates :first_name,           format: { with: /\A[ぁ-んァ-ン一-龥]/, message:"は全角で入力してください。" }
     validates :family_name_reading,  format: { with: /\A[ァ-ヶー－]+\z/, message:"は全角カナで入力してください。" }
     validates :first_name_reading,   format: { with: /\A[ァ-ヶー－]+\z/, message:"は全角カナで入力してください。" }
-    validates :date_of_birth,        numericality: { other_than:1 }
-    #プルダウン選択時に「--」(つまりid:1)の時は保存を受け付けない
+    validates :date_of_birth
   end
 end
