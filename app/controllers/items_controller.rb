a20bc336f500b472c478158121dc3122170d9395
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-07 12:10:06 +0900

    ストロングパラメーターの記述(itemsコントローラー)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,21 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :image,
+      :name,
+      :text,
+      :item_category_id,
+      :item_condition_id,
+      :shipping_charge_id,
+      :shipping_origin_id,
+      :shipping_period_id,
+      :price).merge(
+        user:current_user.id
+      )
+  end
 end
