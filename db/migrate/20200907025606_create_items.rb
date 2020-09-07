526dd7854301f006b7af6258e011e54867a5324c
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-07 12:23:04 +0900

    itemsテーブルのカラム名修正

--- db/migrate/20200907025606_create_items.rb
+++ db/migrate/20200907025606_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :name,             null: false
-      t.text :text,               null: false
-      t.integer :item_category,   null: false
-      t.integer :item_condition,  null: false
-      t.integer :shipping_charge, null: false
-      t.integer :shipping_origin, null: false
-      t.integer :shipping_period, null: false
-      t.integer :price,           null: false
-      t.references :user,         null: false, foreign_key: true
+      t.string :name,                null: false
+      t.text :text,                  null: false
+      t.integer :item_category_id,   null: false
+      t.integer :item_condition_id,  null: false
+      t.integer :shipping_charge_id, null: false
+      t.integer :shipping_origin_id, null: false
+      t.integer :shipping_period_id, null: false
+      t.integer :price,              null: false
+      t.references :user,            null: false, foreign_key: true
       t.timestamps
     end
   end
