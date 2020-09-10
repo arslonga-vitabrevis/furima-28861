ceb3fd79696a29233f095b407b51460b8dcae9c9
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-10 21:38:37 +0900

    showアクションに@itemのインスタンスを予め渡せるよう定義(itemsコントローラー)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show]
+
   def index
     @items = Item.all.includes(:user).order("created_at DESC")
     @orders = Order.all.includes(:item, :user)
@@ -20,6 +22,9 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
 
   def item_params
@@ -33,5 +38,9 @@ class ItemsController < ApplicationController
       :shipping_origin_id,
       :shipping_period_id,
       :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
