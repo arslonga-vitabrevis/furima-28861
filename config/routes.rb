3c271b3b04d8c2d11741bf0b5637c4e4e2816116
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-11 23:51:45 +0900

    ルーティングのネスト設定(itemsの中にordersを入れる)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:new, :create, :index, :edit, :show, :update, :destroy] do
+  resources :items do
+    resources :orders, only: [:index, :create]
   end
 end
