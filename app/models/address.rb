9782d4ac8b6ba0112951a00cfc35758bc4baf336
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-12 14:17:05 +0900

    Addressモデルのバリデーション、アソシエーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,15 @@
 class Address < ApplicationRecord
+  belongs_to :order
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
+  with_options presence: true do
+    validates :postal_code
+    validates :prefecture_id,    numericality: { other_than: 0 }
+    validates :city
+    validates :house_number
+    validates :telephone_number
+    validates :order_id
+  end
 end
