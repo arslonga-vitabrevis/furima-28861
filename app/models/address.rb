a2ea9cad689cfe5f7d44cb5bc4faecffd9547d30
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-12 14:45:24 +0900

    Addressモデルのバリデーションに正規表現の記述を追加

--- app/models/address.rb
+++ app/models/address.rb
@@ -5,11 +5,11 @@ class Address < ApplicationRecord
   belongs_to_active_hash :prefecture
 
   with_options presence: true do
-    validates :postal_code
+    validates :postal_code,      format: { with: /\A\d{3}[-]\d{4}\z/ }
     validates :prefecture_id,    numericality: { other_than: 0 }
     validates :city
     validates :house_number
-    validates :telephone_number
+    validates :telephone_number, format: { with: /\A\d{11}\z/ }
     validates :order_id
   end
 end
