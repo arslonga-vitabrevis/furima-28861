102e6978c0c47266f646c107179020be88f13acc
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-12 16:15:46 +0900

    AddressモデルとOrderモデルのバリデーション記述をAddressOrderモデルに移動

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,13 +3,4 @@ class Address < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-
-  with_options presence: true do
-    validates :postal_code,      format: { with: /\A\d{3}[-]\d{4}\z/ }
-    validates :prefecture_id,    numericality: { other_than: 0 }
-    validates :city
-    validates :house_number
-    validates :telephone_number, format: { with: /\A\d{11}\z/ }
-    validates :order_id
-  end
 end
