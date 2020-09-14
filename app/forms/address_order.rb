23ee1db3f639a3bdb854198bf54ddc57bc42e8ea
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-14 18:41:40 +0900

    Formオブジェクトの保存の記述の修正

--- app/forms/address_order.rb
+++ app/forms/address_order.rb
@@ -22,8 +22,7 @@ class AddressOrder
   end
 
   def save
-    item = Item.find_by(id: params[:item_id])
-    order = Order.create(item_id: item.id, user_id: current_user.id)
+    order = Order.create(item_id: item_id, user_id: user_id)
     Address.create(postal_code: postal_code,
                   prefecture_id: prefecture_id,
                   city: city,
