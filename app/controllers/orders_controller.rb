862ee6de5d15cb921f9016f79daac2b7a759fc95
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-14 18:36:35 +0900

    ordersコントローラーの注文成功後の遷移先の記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,5 +5,12 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find_by(id:params[:item_id])
+    @order = AddressOrder.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 end
