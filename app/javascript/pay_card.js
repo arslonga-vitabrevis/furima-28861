359a846b2f4244e7abd7ea9dc6850dd42a6b827d
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-15 11:46:41 +0900

    公開鍵の環境変数の微修正

--- app/javascript/pay_card.js
+++ app/javascript/pay_card.js
@@ -1,13 +1,13 @@
 const pay = () => {
-  PayJp.setPublicKey(process.PAYJP_PUBLIC_KEY);                             //テスト用公開鍵
+  PayJp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);                           //テスト用公開鍵
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) =>{
-    e.preventDefault();                                                     //同期通信のキャンセル
+    e.preventDefault();
 
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
 
-    const card = {
+    const card = {                                                            //入力されたカード情報を変数に代入
       number: formData.get("number"),
       cvc: formData.get("cvc"),
       exp_month: formData.get("exp_month"),
@@ -16,18 +16,18 @@ const pay = () => {
 
     PayJp.createToken(card, (status, response) => {
       if (status === 200) {
-        const token = response.id;                                          //レスポンスのHTTPステータスコードが正常(200)なら
-        const renderDom = document.getElementById("charge-form");           //生成されたトークンを変数に代入する
-        const tokenObj = `<input value=${token} type="hidden" name='token>`;//隠し要素としてトークンの入った１行フォームを差し込み送信
-        renderDom.insertAdjacentHTML("beforeend", tokenObj);                //隠し要素の差込先→'beforeend'←element 内部の最後の子要素の後に挿入
+        const token = response.id;                                            //レスポンスのHTTPステータスコードが正常(200)なら
+        const renderDom = document.getElementById("charge-form");             //生成されたトークンを変数に代入する
+        const tokenObj = `<input value=${token} type="hidden" name='token'>`; //隠し要素としてトークンの入った１行フォームを差し込み送信
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);                  //隠し要素の差込先→'beforeend'←element 内部の最後の子要素の後に挿入
       }
-        document.getElementById("card-number").removeAttribute("name");     //以下４行は、入力された各カード情報がパラメータとして
-        document.getElementById("card-cvc").removeAttribute("name");        //送信されないようにname要素を削除している
+        document.getElementById("card-number").removeAttribute("name");       //以下４行は、入力された各カード情報がパラメータとして
+        document.getElementById("card-cvc").removeAttribute("name");          //送信されないようにname要素を削除している
         document.getElementById("card-exp-month").removeAttribute("name");
         document.getElementById("card-exp-year").removeAttribute("name");
 
-        document.getElementById("charge-form").submit();                    //フォームに入力された情報をサーバー側に送信
-        document.getElementById("charge-form").reset();                     //入力したものを空にする
+        document.getElementById("charge-form").submit();                      //フォームに入力された情報をサーバー側に送信
+        document.getElementById("charge-form").reset();                       //入力したものを空にする
     });
   });
 };
--- config/initializers/webpacker.rb
+++ config/initializers/webpacker.rb
@@ -1,1 +1,1 @@
-Webpacker::Compiler.PAYJP_PUBLIC_KEY = Rails.application.credentials.payjp_keys[:payjp_public_key]
+Webpacker::Compiler.env["PAYJP_PUBLIC_KEY"] = Rails.application.credentials.payjp_keys[:payjp_public_key]
