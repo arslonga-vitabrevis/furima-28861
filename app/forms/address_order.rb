b105ed79c535ed6accb6c90d5f49ca470962b791
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-15 11:42:57 +0900

    tokenの内部処理とバリデーション
    カラムが存在しない値(token)をインスタンス変数として扱う

--- app/forms/address_order.rb
+++ app/forms/address_order.rb
@@ -1,6 +1,7 @@
 class AddressOrder
   include ActiveModel::Model
-  attr_accessor :postal_code,
+  attr_accessor :token,
+                :postal_code,
                 :prefecture_id,
                 :city,
                 :house_number,
@@ -11,11 +12,12 @@ class AddressOrder
                 :user_id
 
   with_options presence: true do
+    validates :token
     validates :postal_code,      format: { with: /\A\d{3}[-]\d{4}\z/ }
     validates :prefecture_id,    numericality: { other_than: 0 }
     validates :city
     validates :house_number
-    validates :telephone_number, format: { with: /\A\d{11}\z/ }
+    validates :telephone_number, format: { with: /\A\d{10,11}\z/ }
     validates :order_id
     validates :item_id
     validates :user_id
