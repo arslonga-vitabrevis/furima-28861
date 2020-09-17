c3fe3415b0457bd01afee6812c79857d22ea51c0
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-17 12:06:46 +0900

    重複テストケースの消去

--- spec/forms/address_order_spec.rb
+++ spec/forms/address_order_spec.rb
@@ -10,34 +10,6 @@ RSpec.describe AddressOrder, type: :model do
       it '全ての値が正しく入力されていれば保存されること' do
         expect(@order).to be_valid
       end
-      it 'トークンが生成されていること' do
-        @order.token = "tok_5ca06b51685e001723a2c3b4aeb4"
-        expect(@order).to be_valid
-      end
-      it '郵便番号がハイフン付きの7桁の数字であるなら保存されること' do
-        @order.postal_code = "123-4567"
-        expect(@order).to be_valid
-      end
-      it '都道府県が選択されていれば保存されること' do
-        @order.prefecture_id = 13
-        expect(@order).to be_valid
-      end
-      it '市町村が入力されていれば保存されること' do
-        @order.city = "新宿区"
-        expect(@order).to be_valid
-      end
-      it '番地が入力されていれば保存されること' do
-        @order.house_number = "西新宿２丁目８−１"
-        expect(@order).to be_valid
-      end
-      it '建物名が入力されていれば保存されること' do
-        @order.building_name = "東京都本庁舎"
-        expect(@order).to be_valid
-      end
-      it '電話番号がハイフンなしで11桁以内であるなら保存されること' do
-        @order.telephone_number = "12345678901"
-        expect(@order).to be_valid
-      end
     end
 
     context '商品の注文が上手くいかない場合' do
