b320ae5e3c031f963673e1c25b53cb576f4e7dd1
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-24 17:10:01 +0900

    売却後の商品を削除できるようにする
    （売却後の商品を消してもいいのか分からないが一応実装）

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,5 @@
 class Address < ApplicationRecord
-  belongs_to :order
+  belongs_to :order, dependent: :destroy
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
